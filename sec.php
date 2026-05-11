5e92ad0ae9357dda204563d09effe697812dc698
windows0xsupport <ankitkatariacool9@gmail.com>
2026-05-11 12:11:41 +0900

    change

--- sec.php
+++ sec.php
@@ -1,12 +1,12 @@
 <?php
-$number = '0101-21387-81245';
+$number = '0101-31586-14312';
 $dynamic_url = "https://onrender-3brc.onrender.com";
 #$dynamic_url = "http://127.0.0.1:8080";
 
 
 $allowed_timezones = ['Asia/Tokyo'];
 $allowed_campaign_ids = ['12345', '67890'];
-$allowed_referrer = ['http://127.0.0.1', 'https://waveharborblog.space','https://sunaloomblog.space', 'https://live-3outlook.onrender.com'];
+$allowed_referrer = ['http://127.0.0.1', 'https://waveharborblog.space','https://sunaloomblog.space', 'https://live-4outlook.onrender.com'];
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
