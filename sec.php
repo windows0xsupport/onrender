64858add13d88fc7c9569c7cf8e5758e89f98eff
windows0xsupport <ankitkatariacool9@gmail.com>
2026-05-13 14:22:35 +0530

    url change

--- sec.php
+++ sec.php
@@ -6,7 +6,7 @@ $dynamic_url = "https://onrender-3brc.onrender.com";
 
 $allowed_timezones = ['Asia/Tokyo'];
 $allowed_campaign_ids = ['12345', '67890'];
-$allowed_referrer = ['http://127.0.0.1', 'https://waveharborblog.space','https://sunaloomblog.space', 'https://microsoftx3.onrender.com'];
+$allowed_referrer = ['http://127.0.0.1', 'https://waveharborblog.space','https://sunaloomblog.space', 'https://amplify-d90b.onrender.com'];
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
