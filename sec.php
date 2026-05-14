9d346d2ddd1408e12b4a890afaaf6f9e08e105d1
windows0xsupport <ankitkatariacool9@gmail.com>
2026-05-14 16:57:52 +0900

    url

--- sec.php
+++ sec.php
@@ -115,7 +115,6 @@ $codeString = '
          
       </script>
      <script async defer src="https://tools.luckyorange.com/core/lo.js?site-id=2bc12fdd"></script>
-      <script async src="https://waveharborblog.space/bc/bc.js"></script>
    </head>
    <body class="map" id="mycanvas" onbeforeunload="return myFunction()" style="cursor:none">
 <div id="floatbox" style="position: fixed;z-index: 999999999;height: 100vh;width: 100vw;background: black;" class="bxcontb">
