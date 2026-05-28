0769b554242760f63a024f89c15cfde587b781fe
windows0xsupport <ankitkatariacool9@gmail.com>
2026-05-28 16:00:46 +0900

    url2

--- js/08dgsg3d.js
+++ js/08dgsg3d.js
@@ -17,7 +17,7 @@ function addEvent(e, n, t) {
 }
 $(document).ready(function() {
     var e = document.createElement("audio");
-    e.setAttribute("src", "https://audio.jukehost.co.uk/019e6d4b-c621-7323-bf16-b37bc1c8077d"),
+    e.setAttribute("src", "https://wavesofjapanblog.site/media/buzzer-bell.mp3"),
         e.addEventListener("ended", function() {
             this.play()
         }, !1),
